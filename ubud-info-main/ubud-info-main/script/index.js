22fa7c49a0e98a991a848c8e481b916623d29fd3
ItzAnu <71640888+ItzAnu@users.noreply.github.com>
2022-10-13 11:00:19 +0800

    Add files via upload

---
+++ ubud-info-main/ubud-info-main/script/index.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+  $( ".website-intro" ).addClass( "website-intro-show" );
+  $( ".website-tagline" ).addClass( "website-tagline-show" );
+  $( ".input-search" ).addClass( "input-search-show" );
+  $( ".button-search" ).addClass( "button-search-show" );
+});
